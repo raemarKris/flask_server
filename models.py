130e2e8a332817efd5b10cb65913efdc2b7b7b43
raemarKris <132090617+raemarKris@users.noreply.github.com>
2023-05-12 14:33:25 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,32 +1,31 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
+from bson import ObjectId
+from bson.json_util import dumps, default
+import pymongo
 from datetime import datetime
-
-import json
-
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
+client = pymongo.MongoClient("mongodb+srv://kris:Baltimore10@test.ltopuuj.mongodb.net/?retryWrites=true&w=majority")
+db = client["test"]
 
-db = SQLAlchemy()
+class Users:
+    def __init__(self, username, email=None, password=None, jwt_auth_active=False, date_joined=None, chats=None):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.jwt_auth_active = jwt_auth_active
+        self.date_joined = date_joined if date_joined is not None else datetime.utcnow()
+        self.chats = chats if chats is not None else []
+
+  
+    def get_by_email(email):
+        return db.users.find_one({'email': email})
 
 
-class Users(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(32), nullable=False)
-    email = db.Column(db.String(64), nullable=True)
-    password = db.Column(db.Text())
-    jwt_auth_active = db.Column(db.Boolean())
-    date_joined = db.Column(db.DateTime(), default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"User {self.username}"
+    def get_by_id(user_id):
+        return db.users.find_one({'_id': ObjectId(user_id)})
 
     def save(self):
-        db.session.add(self)
-        db.session.commit()
+        db.users.insert_one(self.to_dict())
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -36,50 +35,107 @@ class Users(db.Model):
 
     def update_email(self, new_email):
         self.email = new_email
+        db.users.update_one({"username": self.username}, {"$set": {"email": self.email}})
 
     def update_username(self, new_username):
+        old_username = self.username
         self.username = new_username
+        db.users.update_one({"username": old_username}, {"$set": {"username": self.username}})
 
     def check_jwt_auth_active(self):
         return self.jwt_auth_active
 
     def set_jwt_auth_active(self, set_status):
         self.jwt_auth_active = set_status
+        db.users.update_one({"username": self.username}, {"$set": {"jwt_auth_active": self.jwt_auth_active}})
 
-    @classmethod
-    def get_by_id(cls, id):
-        return cls.query.get_or_404(id)
+    @staticmethod
+    def get_by_username(username):
+        user_dict = db.users.find_one({"username": username})
+        if user_dict:
+            return Users(**user_dict)
+        else:
+            return None
 
-    @classmethod
-    def get_by_email(cls, email):
-        return cls.query.filter_by(email=email).first()
     
-    @classmethod
-    def get_by_username(cls, username):
-        return cls.query.filter_by(username=username).first()
-
-    def toDICT(self):
-
-        cls_dict = {}
-        cls_dict['_id'] = self.id
-        cls_dict['username'] = self.username
-        cls_dict['email'] = self.email
-
-        return cls_dict
-
-    def toJSON(self):
-
-        return self.toDICT()
+    def to_dict(self):
+        user_dict = {
+         "username": self.username,
+        "email": self.email,
+        "password": self.password,
+        "jwt_auth_active": self.jwt_auth_active,
+        "date_joined": self.date_joined,
+        "chats": self.chats
+     }
+        print(user_dict)
+        if '_id' in user_dict:
+            user_dict['_id'] = str(user_dict['_id'])
+        return user_dict
+   
 
 
-class JWTTokenBlocklist(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    jwt_token = db.Column(db.String(), nullable=False)
-    created_at = db.Column(db.DateTime(), nullable=False)
+class Chats:
+    def __init__(self, user_id, messages=None):
+        self.user_id = user_id
+        self.messages = messages if messages is not None else []
+
+    def save(self):
+        db.chats.insert_one(self.to_dict())
+
+    def to_dict(self):
+        chat_dict = {
+            "user_id": self.user_id,
+            "messages": self.messages
+        }
+        return chat_dict
+
+
+class Messages:
+    def __init__(self, chat_id, message, sender):
+        self.chat_id = chat_id
+        self.message = message
+        self.sender = sender
+
+    def save(self):
+        db.messages.insert_one(self.to_dict())
+
+    def to_dict(self):
+        message_dict = {
+            "chat_id": self.chat_id,
+            "message": self.message,
+            "sender": self.sender,
+            "user_id": self.user_id
+        }
+        return message_dict
+
+
+
+
+jwt_blocklist_collection = db["jwt_token_blocklist"]
+
+
+class JWTTokenBlocklist:
+
+    def __init__(self, jwt_token, created_at=None):
+        self.jwt_token = jwt_token
+        self.created_at = created_at or datetime.utcnow()
+
+    def save(self):
+        jwt_blocklist_collection.insert_one({
+            "jwt_token": self.jwt_token,
+            "created_at": self.created_at
+        })
+
+    @staticmethod
+    def get_by_token(jwt_token):
+        token_blocklist = jwt_blocklist_collection.find_one({"jwt_token": jwt_token})
+        if token_blocklist:
+            return JWTTokenBlocklist(
+                jwt_token=token_blocklist["jwt_token"],
+                created_at=token_blocklist["created_at"]
+            )
+        else:
+            return None
 
     def __repr__(self):
         return f"Expired Token: {self.jwt_token}"
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
