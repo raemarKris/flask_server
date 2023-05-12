7be4cac0c8e326df576b266cb1a1a3c03d7372c4
raemarKris <132090617+raemarKris@users.noreply.github.com>
2023-05-12 14:32:41 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from pprint import pformat
 import yaml
 import os, random, string
 from datetime import timedelta
+import pymongo
 # Load config items from config.yaml.
 # Use Path.resolve() to get the absolute path of the parent directory
 yaml_dir = Path(__file__).resolve().parent
@@ -36,57 +37,66 @@ SERVER_PORT = yaml_config.get("SERVER_PORT", None)
 SERVER_DIR = Path(__file__).resolve().parent
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-class BaseConfig():
+
+import os
 
 
+
+class BaseConfig:
     
     SECRET_KEY = os.getenv('SECRET_KEY', None)
     if not SECRET_KEY:
-        SECRET_KEY = ''.join(random.choice( string.ascii_lowercase  ) for i in range( 32 ))
+        SECRET_KEY = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
 
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', None)
     if not JWT_SECRET_KEY:
-        JWT_SECRET_KEY = ''.join(random.choice( string.ascii_lowercase  ) for i in range( 32 ))
+        JWT_SECRET_KEY = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
 
-    GITHUB_CLIENT_ID     = os.getenv('GITHUB_CLIENT_ID' , None)
-    GITHUB_CLIENT_SECRET = os.getenv('GITHUB_SECRET_KEY', None)
-    
+    # GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID', None)
+    # GITHUB_CLIENT_SECRET = os.getenv('GITHUB_SECRET_KEY', None)
+
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 
+    MONGO_URI ='mongodb+srv://kris:Baltimore10@test.ltopuuj.mongodb.net/?retryWrites=true&w=majority'
+
+    if not MONGO_URI:
+        raise ValueError('MONGO_URI environment variable is not set.')
+
+    # pymongo connection options
+    MONGO_OPTIONS = {
+        'retryWrites': True,
+        'w': 'majority'
+    }
+
+    # create a pymongo client instance
+    MONGO_CLIENT = pymongo.MongoClient(MONGO_URI, **MONGO_OPTIONS)
+
+    # extract database name from the uri
+    DB_NAME = pymongo.uri_parser.parse_uri(MONGO_URI)['database']
+
+    # set up the Flask app configuration
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    USE_SQLITE = False
 
-    DB_ENGINE   = os.getenv('DB_ENGINE'   , None)
-    DB_USERNAME = os.getenv('DB_USERNAME' , None)
-    DB_PASS     = os.getenv('DB_PASS'     , None)
-    DB_HOST     = os.getenv('DB_HOST'     , None)
-    DB_PORT     = os.getenv('DB_PORT'     , None)
-    DB_NAME     = os.getenv('DB_NAME'     , None)
+    if not MONGO_URI.startswith('mongodb+srv://'):
+        # parse the uri to extract the username, password, and other parameters
+        parsed_uri = pymongo.uri_parser.parse_uri(MONGO_URI)
+        username = parsed_uri['username']
+        password = parsed_uri['password']
+        auth_source = parsed_uri['options'].get('authSource', DB_NAME)
 
-    USE_SQLITE  = True 
+        # create a new uri with url-encoded username and password
+        quoted_username = quote_plus(username)
+        quoted_password = quote_plus(password)
+        new_uri = f"mongodb://{quoted_username}:{quoted_password}@{parsed_uri['nodelist'][0][0]}:{parsed_uri['nodelist'][0][1]}/{DB_NAME}?authSource={auth_source}"
 
-    # try to set up a Relational DBMS
-    if DB_ENGINE and DB_NAME and DB_USERNAME:
+        # set the new uri as the connection uri
+        MONGO_URI = new_uri
 
-        try:
-            
-            # Relational DBMS: PSQL, MySql
-            SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-                DB_ENGINE,
-                DB_USERNAME,
-                DB_PASS,
-                DB_HOST,
-                DB_PORT,
-                DB_NAME
-            ) 
+    # print the connection uri for debugging purposes
+    print(f"Connecting to MongoDB at {MONGO_URI}")
 
-            USE_SQLITE  = False
-
-        except Exception as e:
-
-            print('> Error: DBMS Exception: ' + str(e) )
-            print('> Fallback to SQLite ')    
-
-    if USE_SQLITE:
-
-        # This will create a file in <app> FOLDER
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    # set up the Flask app configuration
+    MONGODB_SETTINGS = {
+        'host': MONGO_URI,
+    }
